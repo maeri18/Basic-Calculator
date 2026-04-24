d9a52d57d8a9d6944a73f3bd6447d7f9ed3bd349
Chekam Ingrid Maéva <91965635+maeri18@users.noreply.github.com>
2026-04-24 14:08:56 +0200

    Fix subtraction method to perform addition

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ class Calculator:
         raise ValueError("Tried to divide by zero!")
         
     def sub(self, num1, num2):
-        return num1 -  num2
+        return num1 + num2
 
     def mul(self, num1, num2):
         return num1 * num2
