f1bb76326e7af43cd6a254d08966d1b5a1c8a4a5
Chekam Ingrid Maéva <91965635+maeri18@users.noreply.github.com>
2026-04-24 16:34:49 +0200

    Fix division by zero and update subtraction method
    Added error handling for division by zero and corrected subtraction logic.

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ class Calculator:
         return num1 + num2
     
     def div(self, num1, num2):
-        #if(num2 != 0):
-        return num1 / num2
+        if(num2 != 0):
+            return num1 / num2
         #raise ValueError("Tried to divide by zero!")
         
     def sub(self, num1, num2):
-        return num1 + num2
+        return num1 - num2
 
     def mul(self, num1, num2):
         return num1 * num2
